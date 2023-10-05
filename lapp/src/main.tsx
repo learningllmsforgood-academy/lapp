e6752ca2e802829de0dbf3cd184f294c2575d6ed
Abhishek Kedia <kedia.abhishek10@gmail.com>
2023-10-05 10:56:56 +0530

    cleanup

--- lapp/src/main.tsx
+++ lapp/src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
+import ViteStarterApp from './ViteStarterApp.tsx'
 import './index.css'
-import FirebaseLoginUI from './ui/FirebaseLoginUI'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <FirebaseLoginUI />
+    <ViteStarterApp />
   </React.StrictMode>,
 )

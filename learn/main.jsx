529ba5edf530427684e07d1e3d478d5c324e226e
Abhishek Kedia <kedia.abhishek10@gmail.com>
2023-10-12 15:14:37 +0530

    play with state

--- learn/main.jsx
+++ learn/main.jsx
@@ -1,5 +1,33 @@
 const app = document.getElementById("app");
+
+function Header({title}) {
+    return (<h1>{title}</h1>);
+}
+
+function HomePage() {
+    const names = [ "Ada Lovelace", "Alan Turing", "Larry Page" ];
+    const [ likes, setLikes ] = React.useState(0);
+
+    function handleLikeClick() {
+        setLikes(likes + 1);
+    }
+
+    return (
+        <div>
+            <Header title={"Keep Learning! Keep Building! Keep Shipping! 👷 🏫 🚢"}/>
+            <Header title={"React 💙"}/>
+            <ul>
+                {names.map(name => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+
+            <button onClick={handleLikeClick}>Like {likes}</button>
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <h1>Keep Learning! Keep Building! Keep Shipping! 👷 🏫 🚢</h1>,
+    <HomePage />,
     app
 );
